44dfb61d8ab363868b3bf750b8e473c0765d86d1
akmeonuzraa <129215282+akmeonuzraa@users.noreply.github.com>
2025-02-23 16:55:15 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fonction JavaScript pour changer le texte au clic
+function changeText() {
+  const headerText = document.querySelector("header h1");
+  headerText.textContent = "Merci d'avoir cliqué!";
+}
